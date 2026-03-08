7c77180944cd344997cacdbe1643d4bcb8dea856
Raphael Ribeiro <raphael3030@gmail.com>
2026-03-08 16:30:50 -0300

    fix:corrige environments

--- src/app/environment/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  emailjs: {
-    serviceId: 'service_id',
-    templateId: 'template_id', 
-    autoReplyTemplateId: 'template_id_auto_reply', 
-    publicKey: 'public_key'
-  }
-};
