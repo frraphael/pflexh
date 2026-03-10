a5939c42778161da1d6352cabd95ae8a159a2d39
Raphael Ribeiro <raphael3030@gmail.com>
2026-03-09 21:42:42 -0300

    feat:corrige environments

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   emailjs: {
-    serviceId: 'service_id',
-    templateId: 'template_id',
-    autoReplyTemplateId: 'template_id_auto_reply',
-    publicKey: 'public_key'
+    serviceId: 'service_cef3k1d',
+    templateId: 'template_w25f4jc',
+    autoReplyTemplateId: 'template_q56lvos', 
+    publicKey: 'Qt-T16lEX1IOJhiAr'
   }
 };
